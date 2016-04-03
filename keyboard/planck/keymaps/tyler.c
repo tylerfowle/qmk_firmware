f574262d61943504aed927a9ac4efe3b63511c84
Tyler Fowle <tylerfowle@gmail.com>
2016-04-03 17:05:20 -0500

    update tyler keymap

--- keyboard/planck/keymaps/tyler.c
+++ keyboard/planck/keymaps/tyler.c
@@ -12,11 +12,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {M(0),    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,    KC_BSPC},
     {TAB_FN1, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN, KC_ENT},
     {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT},
-    {__NO___, KC_LCTL, KC_LALT, KC_LGUI, MO(1),   _SPACE_, __NO___, MO(2),   __NO___, __NO___,  __NO___, KC_PENT}
+    {__NO___, KC_LCTL, KC_LALT, KC_LGUI, MO(1),   _SPACE_, __NO___, MO(2),   __NO___, __NO___,  KC_BSLS, KC_PENT}
   },
   [1] = { /* FN1 // Left FN */
     {KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  _______, _______, _______, KC_UP,   _______,  SCRNCAP, KC_DELETE},
-    {_______, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, _______, M(1),    KC_LEFT, KC_DOWN, KC_RIGHT, KC_BSLS, KC_QUOT},
+    {_______, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, _______, M(1),    KC_LEFT, KC_DOWN, KC_RIGHT, KC_QUOT, S(KC_QUOT)},
     {_______, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS, _______, _______, _______, KC_PAUS, KC_VOLD,  KC_VOLU, KC_MUTE},
     {_______, _______, _______, _______, _______, _SPACE_, __NO___, _______, KC_SLCK, KC_MPRV,  KC_MNXT, KC_MPLY}
   },
