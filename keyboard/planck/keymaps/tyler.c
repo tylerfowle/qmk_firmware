e420fcb7e3ef68ae15d5990578f0c74f3b8fff68
Tyler Fowle <tylerfowle@gmail.com>
2016-04-04 21:22:24 -0500

    update keymap to include windows layer

--- keyboard/planck/keymaps/tyler.c
+++ keyboard/planck/keymaps/tyler.c
@@ -8,20 +8,27 @@
 #define SCRNCAP LGUI(LSFT(KC_4))
 #define COLORCAP LCTL(LALT(LGUI(KC_C)))
 
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = {  /* Base */
+  [0] = {  /* Base Mac */
     {M(0),    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,    KC_BSPC},
     {TAB_FN1, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN, KC_ENT},
     {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT},
-    {__NO___, KC_LCTL, KC_LALT, KC_LGUI, MO(1),   _SPACE_, __NO___, MO(2),   __NO___, __NO___,  KC_BSLS, KC_PENT}
+    {TG(1),   KC_LCTL, KC_LALT, KC_LGUI, MO(2),   _SPACE_, __NO___, MO(3),   __NO___, __NO___,  KC_BSLS, KC_PENT}
   },
-  [1] = { /* FN1 // Left FN */
+  [1] = {  /* Base Windows */
+    {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______},
+    {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______},
+    {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______},
+    {_______, KC_LGUI, KC_LALT, KC_LCTL, _______, _______, _______, _______, _______, _______,  _______,  _______}
+  },
+  [2] = {  /* Left FN */
     {KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  _______, _______, _______, KC_UP,   _______,  SCRNCAP, KC_DELETE},
     {_______, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, _______, M(1),    KC_LEFT, KC_DOWN, KC_RIGHT, _______, KC_QUOT},
     {_______, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS, COLORCAP,_______, _______, KC_PAUS, KC_VOLD,  KC_VOLU, KC_MUTE},
     {_______, _______, _______, _______, _______, _SPACE_, __NO___, _______, KC_SLCK, KC_MPRV,  KC_MNXT, KC_MPLY}
   },
-  [2] = { /* FN2 // Right FN */
+  [3] = {  /* Right FN */
     {_______, KC_1,    KC_2,    KC_3,    KC_4,    _______, _______, KC_LBRC, KC_RBRC, KC_QUOT,  S(KC_QUOT), KC_BSPC},
     {_______, KC_5,    KC_6,    KC_7,    KC_8,    _______, _______, KC_LCBR, KC_RCBR, KC_MINS,  KC_UNDS,   _______},
     {_______, KC_9,    KC_0,    KC_MINS, KC_EQL,  _______, _______, KC_LPRN, KC_RPRN, _______,  _______,   _______},
