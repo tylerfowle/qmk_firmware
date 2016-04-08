71dab45465c033ada2118a5adc28252b5c893304
Tyler Fowle <tylerfowle@gmail.com>
2016-04-08 12:57:46 -0500

    update plank layout, add desktop switching shortcuts

--- keyboard/planck/keymaps/tyler.c
+++ keyboard/planck/keymaps/tyler.c
@@ -14,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {M(0),    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,    KC_BSPC},
     {TAB_FN1, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN, KC_ENT},
     {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT},
-    {TG(1),   KC_LCTL, KC_LALT, KC_LGUI, MO(2),   _SPACE_, __NO___, MO(3),   __NO___, __NO___,  KC_BSLS, KC_PENT}
+    {TG(1),   KC_LCTL, KC_LALT, KC_LGUI, MO(2),   _SPACE_, __NO___, MO(3),   LALT(KC_1),LALT(KC_2),  KC_BSLS, KC_PENT}
   },
   [1] = {  /* Base Windows */
     {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______},
