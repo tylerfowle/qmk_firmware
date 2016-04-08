2d6e03567349cfafb30fb060fbbab61069682b9a
Tyler Fowle <tylerfowle@gmail.com>
2016-04-08 15:45:52 -0500

    new sebright layout based off planck

---
+++ keyboard/tyler/keymaps/default.c
@@ -0,0 +1,70 @@
+// This is the canonical layout file for the Quantum project. If you want to add another keyboard,
+// this is the style you want to emulate.
+
+#include "tyler.h"
+#include "action_util.h"
+
+
+#define __NO___ KC_NO
+#define _______ KC_TRNS
+#define _SPACE_ KC_SPC
+#define TAB_FN1 LT(2,KC_TAB)
+#define SCRNCAP LGUI(LSFT(KC_4))
+#define COLORCAP LCTL(LALT(LGUI(KC_C)))
+
+
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* 0: base */
+ KEYMAP(
+  M(0),      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,      KC_P,       KC_BSPC, \
+  TAB_FN1,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,     KC_J,     KC_K,     KC_L,      KC_ENT, \
+  KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,    KC_SLSH, \
+      KC_LCTL,   KC_LALT,  KC_LGUI,     _SPACE_,           MO(1),    MO(2),    KC_PAUS,  KC_RSFT),
+    /* 1: numbers and arrows */
+ KEYMAP(
+  _______,   KC_1,    KC_2,    KC_3,    KC_4,    _______,  _______,  _______,  KC_UP,    _______,   SCRNCAP,    KC_DEL, \
+  KC_CAPS,   KC_5,    KC_6,    KC_7,    KC_8,    _______,  M(1),     KC_LEFT,  KC_DOWN,  KC_RIGHT,  _______, \
+  _______,   KC_9,    KC_0,    KC_MINS, KC_EQUAL,COLORCAP, _______,  _______,  KC_VOLD,  KC_VOLU,   KC_MUTE, \
+      _______,   _______,  _______,   _______,             _______,  KC_MPRV,  KC_MNXT,  KC_MPLY),
+    /* 2: symbols */
+ KEYMAP(
+  _______,   KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  _______,  _______,  KC_LBRC,  KC_RBRC,  KC_QUOT,   S(KC_QUOT), KC_BSPC, \
+  _______,   KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, _______,  _______,  KC_LCBR,  KC_RCBR,  KC_MINS,   KC_UNDS, \
+  _______,   KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS, _______,  _______,  KC_LPRN,  KC_RPRN,  _______,   _______, \
+      _______,   _______,  _______,     _______,           _______,  _______,  _______,  _______),
+};
+
+
+
+const uint16_t PROGMEM fn_actions[] = {
+  [1] = ACTION_LAYER_MOMENTARY(1),
+  [2] = ACTION_LAYER_MOMENTARY(2),
+  [3] = ACTION_LAYER_MOMENTARY(3),
+
+  [31] = ACTION_MODS_TAP_KEY(MOD_LSFT, KC_QUOTE),
+};
+
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
+{
+  switch(id) {
+    case 0: {
+      static uint8_t code;
+      if (record->event.pressed) {
+        code = (keyboard_report->mods & (MOD_BIT(KC_LSFT) | MOD_BIT(KC_RSFT))) ? KC_GRV : KC_ESC;
+        register_code(code);
+      }
+      else {
+        unregister_code(code);
+      }
+      break;
+    }
+    break;
+    case 1:
+    if (record->event.pressed) {
+      return MACRO( I(5), D(LSFT), T(SCLN), U(LSFT), T(W), T(Q), T(ENT), END  );
+    }
+    break;
+  }
+  return MACRO_NONE;
+};
---
+++ keyboard/tyler/tyler.h
@@ -0,0 +1,30 @@
+#ifndef TYLER_H
+#define TYLER_H
+
+#include "matrix.h"
+#include "keymap_common.h"
+#include "backlight.h"
+#include <stddef.h>
+
+// This a shortcut to help you visually see your layout.
+// The following is an example using the Planck MIT layout
+// The first section contains all of the arguements
+// The second converts the arguments into a two-dimensional array
+#define KEYMAP( \
+    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0A, k0B,  \
+    k10, k11, k12, k13, k14, k15, k16, k17, k18, k19,      k1A,  \
+    k20,      k22, k23, k24, k25, k26, k27, k28, k29, k2A, k2B,  \
+         k31,      k33, k34, k35,      k37, k38, k39, k3A        \
+) { \
+    { k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0A, k0B }, \
+    { k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, KC_NO,    k1A }, \
+    { k20, KC_NO,    k22, k23, k24, k25, k26, k27, k28, k29, k2A, k2B }, \
+    { KC_NO,    k31, KC_NO,    k33, k34, k35, KC_NO,    k37, k38, k39, k3A, KC_NO    } \
+}
+
+void matrix_init_user(void);
+void matrix_scan_user(void);
+void process_action_user(keyrecord_t *record);
+void led_set_user(uint8_t usb_led);
+
+#endif
