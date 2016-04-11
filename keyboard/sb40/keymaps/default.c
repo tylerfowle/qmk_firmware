b6832dcc8887876b321e96786eafcaf2b8608778
Tyler Fowle <tylerfowle@gmail.com>
2016-04-11 12:53:09 -0500

    update layout

--- keyboard/sb40/keymaps/default.c
+++ keyboard/sb40/keymaps/default.c
@@ -18,13 +18,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {M(0),      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,      KC_P,       KC_BSPC},
     {TAB_FN1,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,     KC_J,     KC_K,     KC_L,      __NO___,    KC_ENT},
     {KC_LSFT,   __NO___, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,   KC_DOT,     KC_SLSH},
-    {__NO___,  KC_LCTL,  __NO___, KC_LALT, KC_LGUI, _SPACE_,  __NO___,  MO(1),    MO(2),    KC_PAUS,   KC_RSFT}
+    {__NO___,  KC_LCTL,  __NO___, KC_LALT, KC_LGUI, _SPACE_,  __NO___,  MO(1),    MO(2),    KC_BSLS,   KC_QUOT}
   },
   /* 1: numbers and arrows */
   [1] = { 
     {_______,   KC_1,    KC_2,    KC_3,    KC_4,    _______,  _______,  _______,  KC_UP,    _______,   SCRNCAP,    KC_DEL},
-    {KC_CAPS,   KC_5,    KC_6,    KC_7,    KC_8,    _______,  M(1),     KC_LEFT,  KC_DOWN,  KC_RIGHT,  __NO___,    KC_QUOT},
-    {_______,   __NO___, KC_9,    KC_0,    KC_MINS, KC_EQUAL, COLORCAP, _______,  _______,  KC_VOLD,   KC_VOLU,    KC_MUTE},
+    {_______,   KC_5,    KC_6,    KC_7,    KC_8,    _______,  M(1),     KC_LEFT,  KC_DOWN,  KC_RIGHT,  __NO___,    KC_QUOT},
+    {KC_CAPS,   __NO___, KC_9,    KC_0,    KC_MINS, KC_EQUAL, COLORCAP, _______,  _______,  KC_VOLD,   KC_VOLU,    KC_MUTE},
     {__NO___,   _______, __NO___, _______,  _______,_______,  __NO___,  _______,  KC_MPRV,  KC_MNXT,   KC_MPLY}
   },
   /* 2: symbols */
