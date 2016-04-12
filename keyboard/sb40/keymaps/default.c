e4351440425c7555d38a96a5903aab08b8af87ec
Tyler Fowle <tylerfowle@gmail.com>
2016-04-12 14:48:56 -0500

    update keymap

--- keyboard/sb40/keymaps/default.c
+++ keyboard/sb40/keymaps/default.c
@@ -1,38 +1,59 @@
-// This is the canonical layout file for the Quantum project. If you want to add another keyboard,
-// this is the style you want to emulate.
-
 #include "sb40.h"
 #include "action_util.h"
 
-
+// for clarity
 #define __NO___ KC_NO
+#define XXXXXXX KC_NO
 #define _______ KC_TRNS
 #define _SPACE_ KC_SPC
+
+// momentary layer or keypress
 #define TAB_FN1 LT(1,KC_TAB)
+#define SPC_FN3 LT(3,KC_SPC)
+
+// shortcuts
 #define SCRNCAP LGUI(LSFT(KC_4))
 #define COLORCAP LCTL(LALT(LGUI(KC_C)))
+
+
+
+// blank layer
+
+// [0] = { 
+// {_______,   _______, _______, _______, _______, _______,  _______,  _______,  _______,  _______,   _______,    _______},
+// {_______,   _______, _______, _______, _______, _______,  _______,  _______,  _______,  _______,   XXXXXXX,    _______},
+// {_______,   XXXXXXX, _______, _______, _______, _______,  _______,  _______,  _______,  _______,   _______,    _______},
+// {XXXXXXX,   _______, XXXXXXX, _______, _______, _______,  XXXXXXX,  _______,  _______,  _______,   _______}
+// }
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = { /* 0: base */
     {M(0),      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,      KC_P,       KC_BSPC},
-    {TAB_FN1,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,     KC_J,     KC_K,     KC_L,      __NO___,    KC_ENT},
-    {KC_LSFT,   __NO___, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,   KC_DOT,     KC_SLSH},
-    {__NO___,  KC_LCTL,  __NO___, KC_LALT, KC_LGUI, _SPACE_,  __NO___,  MO(1),    MO(2),    KC_BSLS,   KC_QUOT}
+    {TAB_FN1,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,     KC_J,     KC_K,     KC_L,      XXXXXXX,    KC_ENTER},
+    {KC_LSFT,   XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,   KC_DOT,     KC_SLSH},
+    {XXXXXXX,   KC_LCTL, XXXXXXX, KC_LALT, KC_LGUI, SPC_FN3,    XXXXXXX,  MO(1),    MO(2),    KC_BSLS,   KC_QUOT}
   },
   /* 1: numbers and arrows */
   [1] = { 
     {KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    _______,  _______,  _______,  KC_UP,    _______,   SCRNCAP,    KC_DEL},
-    {_______,   KC_5,    KC_6,    KC_7,    KC_8,    _______,  M(1),     KC_LEFT,  KC_DOWN,  KC_RIGHT,  __NO___,    KC_SCLN},
-    {_______,   __NO___, KC_9,    KC_0,    KC_MINS, KC_EQUAL, COLORCAP, _______,  _______,  KC_VOLD,   KC_VOLU,    KC_MUTE},
-    {__NO___,   _______, __NO___, _______,  _______,_______,  __NO___,  _______,  KC_MPRV,  KC_MNXT,   KC_MPLY}
+    {_______,   KC_5,    KC_6,    KC_7,    KC_8,    _______,  M(1),     KC_LEFT,  KC_DOWN,  KC_RIGHT,  XXXXXXX,    KC_SCLN},
+    {_______,   XXXXXXX, KC_9,    KC_0,    KC_MINS, KC_EQUAL, COLORCAP, _______,  _______,  KC_VOLD,   KC_VOLU,    KC_MUTE},
+    {XXXXXXX,   _______, XXXXXXX, _______,  _______,_______,  XXXXXXX,  _______,  KC_MPRV,  KC_MNXT,   KC_MPLY}
   },
-  /* 2: symbols */
+  // layer 2: symbols
   [2] = { 
     {_______,   KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  _______,  _______,  KC_LBRC,  KC_RBRC,  KC_QUOT,   S(KC_QUOT), KC_BSPC},
-    {_______,   KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, _______,  _______,  KC_LCBR,  KC_RCBR,  KC_MINS,   __NO___,    KC_UNDS},
-    {_______,   __NO___, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS,  _______,  _______,  KC_LPRN,  KC_RPRN,   _______,    _______},
-    {__NO___,   _______, __NO___, _______, _______, _______,  __NO___,  _______,  _______,   _______,  _______}
+    {_______,   KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, _______,  _______,  KC_LCBR,  KC_RCBR,  KC_MINS,   XXXXXXX,    KC_UNDS},
+    {_______,   XXXXXXX, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS,  _______,  _______,  KC_LPRN,  KC_RPRN,   _______,    _______},
+    {XXXXXXX,   _______, XXXXXXX, _______, _______, _______,  XXXXXXX,  _______,  _______,   _______,  _______}
+  },
+  // layer 3
+  [3] = { 
+    {_______,   _______, _______, _______, _______, _______,  _______,  _______,  _______,  _______,   _______,    _______},
+    {_______,   _______, _______, _______, _______, _______,  _______,  _______,  _______,  _______,   XXXXXXX,    S(KC_SCLN)},
+    {_______,   XXXXXXX, _______, _______, _______, _______,  _______,  _______,  _______,  _______,   _______,    _______},
+    {XXXXXXX,   _______, XXXXXXX, _______, _______, _______,  XXXXXXX,  _______,  _______,   _______,  _______}
   }
 };
 
@@ -58,9 +79,10 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
       break;
     }
     break;
-    case 1:
-    if (record->event.pressed) {
-      return MACRO( I(5), D(LSFT), T(SCLN), U(LSFT), T(W), T(Q), T(ENT), END  );
+    case 1: {
+      if (record->event.pressed) {
+        return MACRO( I(5), T(ESC), D(LSFT), T(SCLN), U(LSFT), T(W), T(Q), T(ENT), END  );
+      }
     }
     break;
   }
