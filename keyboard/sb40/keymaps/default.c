4e49c4b3de546430f1918ce0be055fdd818ad753
Tyler Fowle <tylerfowle@gmail.com>
2016-04-12 15:47:06 -0500

    move media keys to a new layer

--- keyboard/sb40/keymaps/default.c
+++ keyboard/sb40/keymaps/default.c
@@ -8,8 +8,7 @@
 #define _SPACE_ KC_SPC
 
 // momentary layer or keypress
-#define TAB_FN1 LT(1,KC_TAB)
-#define SPC_FN3 LT(3,KC_SPC)
+#define TAB_FN3 LT(3,KC_TAB)
 
 // shortcuts
 #define SCRNCAP LGUI(LSFT(KC_4))
@@ -30,30 +29,30 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = { /* 0: base */
     {M(0),      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,      KC_P,       KC_BSPC},
-    {TAB_FN1,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,     KC_J,     KC_K,     KC_L,      XXXXXXX,    KC_ENTER},
+    {TAB_FN3,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,     KC_J,     KC_K,     KC_L,      XXXXXXX,    KC_ENTER},
     {KC_LSFT,   XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,   KC_DOT,     KC_SLSH},
-    {XXXXXXX,   KC_LCTL, XXXXXXX, KC_LALT, KC_LGUI, SPC_FN3,    XXXXXXX,  MO(1),    MO(2),    KC_BSLS,   KC_QUOT}
+    {XXXXXXX,   KC_LCTL, XXXXXXX, KC_LALT, KC_LGUI, _SPACE_,  XXXXXXX,  MO(1),    MO(2),    KC_SCLN,   KC_QUOT}
   },
   /* 1: numbers and arrows */
   [1] = { 
     {KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    _______,  _______,  _______,  KC_UP,    _______,   SCRNCAP,    KC_DEL},
-    {_______,   KC_5,    KC_6,    KC_7,    KC_8,    _______,  M(1),     KC_LEFT,  KC_DOWN,  KC_RIGHT,  XXXXXXX,    KC_SCLN},
-    {_______,   XXXXXXX, KC_9,    KC_0,    KC_MINS, KC_EQUAL, COLORCAP, _______,  _______,  KC_VOLD,   KC_VOLU,    KC_MUTE},
-    {XXXXXXX,   _______, XXXXXXX, _______,  _______,_______,  XXXXXXX,  _______,  KC_MPRV,  KC_MNXT,   KC_MPLY}
+    {_______,   KC_5,    KC_6,    KC_7,    KC_8,    _______,  M(1),     KC_LEFT,  KC_DOWN,  KC_RIGHT,  XXXXXXX,    KC_BSLS},
+    {_______,   XXXXXXX, KC_9,    KC_0,    KC_MINS, KC_EQUAL, COLORCAP, _______,  _______,  _______,   _______,    _______},
+    {XXXXXXX,   _______, XXXXXXX, _______,  _______,_______,  XXXXXXX,  _______,  _______,  _______,   _______}
   },
   // layer 2: symbols
   [2] = { 
-    {_______,   KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  _______,  _______,  KC_LBRC,  KC_RBRC,  KC_QUOT,   S(KC_QUOT), KC_BSPC},
+    {_______,   KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  _______,  _______,  KC_LBRC,  KC_RBRC,  _______,   _______,    KC_BSPC},
     {_______,   KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, _______,  _______,  KC_LCBR,  KC_RCBR,  KC_MINS,   XXXXXXX,    KC_UNDS},
     {_______,   XXXXXXX, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS,  _______,  _______,  KC_LPRN,  KC_RPRN,   _______,    _______},
-    {XXXXXXX,   _______, XXXXXXX, _______, _______, _______,  XXXXXXX,  _______,  _______,   _______,  _______}
+    {XXXXXXX,   _______, XXXXXXX, _______, _______, _______,  XXXXXXX,  _______,  _______,  _______,  _______}
   },
-  // layer 3
+  // layer 3: media keys
   [3] = { 
     {_______,   _______, _______, _______, _______, _______,  _______,  _______,  _______,  _______,   _______,    _______},
-    {_______,   _______, _______, _______, _______, _______,  _______,  _______,  _______,  _______,   XXXXXXX,    S(KC_SCLN)},
-    {_______,   XXXXXXX, _______, _______, _______, _______,  _______,  _______,  _______,  _______,   _______,    _______},
-    {XXXXXXX,   _______, XXXXXXX, _______, _______, _______,  XXXXXXX,  _______,  _______,   _______,  _______}
+    {_______,   _______, _______, _______, _______, _______,  _______,  _______,  _______,  _______,   XXXXXXX,    _______},
+    {_______,   XXXXXXX, _______, _______, _______, _______,  _______,  _______,  KC_MUTE,  KC_VOLD,   KC_VOLU,    _______},
+    {XXXXXXX,   _______, XXXXXXX, _______, _______, _______,  XXXXXXX,  _______,  KC_MPRV,   KC_MNXT,  KC_MPLY}
   }
 };
 
