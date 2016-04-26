b087f1d3cca8a5f4d5e82a8b0f9add42dbcf4278
Tyler Fowle <tylerfowle@gmail.com>
2016-04-26 09:50:00 -0500

    update layout, left hand fn2, middle quote

--- keyboard/bw45/keymaps/default.c
+++ keyboard/bw45/keymaps/default.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {M(0),      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     XXXXXXX,  KC_Y,     KC_U,     KC_I,     KC_O,      KC_P,      KC_BSPC },
     {TAB_FN,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     XXXXXXX,  KC_H,     KC_J,     KC_K,     KC_L,      KC_SCLN,   KC_ENTER},
     {KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     XXXXXXX,  KC_N,     KC_M,     KC_COMM,  KC_DOT,    KC_SLSH,   KC_RSFT },
-    {XXXXXXX,   XXXXXXX, KC_LCTL, KC_LALT, KC_LGUI, SPC_FN1,  SPC_FN2,  _SPACE_,  MO(1),    MO(2),    KC_BSLS,   XXXXXXX,   XXXXXXX }
+    {XXXXXXX,   XXXXXXX, KC_LCTL, KC_LALT, KC_LGUI, SPC_FN2,  KC_QUOT,  _SPACE_,  MO(1),    MO(2),    KC_BSLS,   XXXXXXX,   XXXXXXX }
   },
   // layer 1: numbers
   [1] = { 
@@ -54,29 +54,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [3] = { 
     {_______,   _______, _______, _______, _______, _______,  XXXXXXX,  _______,  _______,  KC_UP,    COLORCAP,  SCRNCAP,   KC_DEL  },
     {_______,   LEFTSCR, RIGHSCR, COLORCAP,_______, _______,  XXXXXXX,  _______,  KC_LEFT,  KC_DOWN,  KC_RIGHT,  _______,   M(1)    },
-    {_______,   _______, _______, SCRNCAP, _______, _______,  XXXXXXX,  _______,  KC_MUTE,  KC_VOLD,   KC_VOLU,  _______,   _______ },
+    {_______,   _______, _______, SCRNCAP, _______, _______,  XXXXXXX,  _______,  KC_MUTE,  KC_VOLD,  KC_VOLU,   _______,   _______ },
     {XXXXXXX,   XXXXXXX, _______, _______, _______, _______,  _______,  _______,  KC_MPRV,  KC_MNXT,  KC_MPLY,   XXXXXXX,   XXXXXXX }
   },
-  // [1] = { 
-  //   {KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    _______,  _______,  _______,  _______,  _______,   _______,    KC_DEL },
-  //   {_______,   KC_5,    KC_6,    KC_7,    KC_8,    _______,  _______,  KC_QUOT,  S(KC_QUOT), KC_COLN,   XXXXXXX,  KC_SCLN},
-  //   {_______,   XXXXXXX, KC_9,    KC_0,    KC_MINS, KC_EQUAL, _______,  _______,  _______,  _______,   _______,    _______},
-  //   {XXXXXXX,   _______, XXXXXXX, _______,  _______,_______,  XXXXXXX,  _______,  _______,  _______,   _______}
-  // },
-  // // layer 2: symbols and punc
-  // [2] = { 
-  //   {_______,   KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  _______,  _______,  KC_LBRC,  KC_RBRC,  _______,   _______,    KC_BSPC},
-  //   {_______,   KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, _______,  _______,  KC_LCBR,  KC_RCBR,  KC_MINS,   XXXXXXX,    KC_UNDS},
-  //   {_______,   XXXXXXX, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS,  _______,  _______,  KC_LPRN,  KC_RPRN,   _______,    _______},
-  //   {XXXXXXX,   _______, XXXXXXX, _______, _______, _______,  XXXXXXX,  _______,  _______,  _______,   _______}
-  // },
-  // // layer 3: arrow, media, and shortcut keys
-  // [3] = { 
-  //   {_______,   _______, _______, _______, _______, _______,  _______,  _______,  KC_UP,    COLORCAP,  SCRNCAP,    KC_DEL },
-  //   {_______,   LEFTSCR, RIGHSCR, _______, _______, _______,  _______,  KC_LEFT,  KC_DOWN,  KC_RIGHT,  XXXXXXX,    M(1)   },
-  //   {_______,   XXXXXXX, _______, _______, _______, _______,  _______,  _______,  KC_MUTE,  KC_VOLD,   KC_VOLU,    _______},
-  //   {XXXXXXX,   _______, XXXXXXX, _______, _______, _______,  XXXXXXX,  _______,  KC_MPRV,  KC_MNXT,   KC_MPLY}
-  // }
 };
 
 
