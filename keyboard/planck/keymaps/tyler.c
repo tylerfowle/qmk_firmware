452db27a43fea9635247bccf248aa84c3b1cb899
Tyler Fowle <tylerfowle@gmail.com>
2016-05-18 09:52:03 -0500

    update plank layout to match bw45

--- keyboard/planck/keymaps/tyler.c
+++ keyboard/planck/keymaps/tyler.c
@@ -4,35 +4,62 @@
 #define __NO___ KC_NO
 #define _______ KC_TRNS
 #define _SPACE_ KC_SPC
-#define TAB_FN1 LT(2,KC_TAB)
+
+#define TAB_FN LT(4,KC_TAB)
 #define SCRNCAP LGUI(LSFT(KC_4))
 #define COLORCAP LCTL(LALT(LGUI(KC_C)))
 
+#define LEFTSCR LALT(KC_1)
+#define RIGHSCR LALT(KC_2)
+
+// Blank Layer
+
+// layer 1: Description
+// [1] = {
+//   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,   _______},
+//   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,   _______},
+//   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,   _______},
+//   {_______, _______, _______, _______, _______, _SPACE_, __NO___, _______, _______, _______,  _______,   _______}
+// }
+
+
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = {  /* Base Mac */
+  // layer 1: base mac
+  [0] = {
     {M(0),    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,    KC_BSPC},
-    {TAB_FN1, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN, KC_ENT},
+    {TAB_FN,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN, KC_ENT},
     {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT},
-    {TG(1),   KC_LCTL, KC_LALT, KC_LGUI, MO(2),   _SPACE_, __NO___, MO(3),   LALT(KC_1),LALT(KC_2),  KC_BSLS, KC_PENT}
+    {TG(1),   KC_LCTL, KC_LALT, KC_LGUI, MO(3),   _SPACE_, __NO___, MO(2),   _______, KC_QUOT,  KC_BSLS, _______}
   },
-  [1] = {  /* Base Windows */
+  // layer 1: base windows
+  [1] = {
     {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______},
     {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______},
     {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______},
     {_______, KC_LGUI, KC_LALT, KC_LCTL, _______, _______, _______, _______, _______, _______,  _______,  _______}
   },
-  [2] = {  /* Left FN */
-    {KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  _______, _______, _______, KC_UP,   _______,  SCRNCAP, KC_DELETE},
-    {_______, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, _______, M(1),    KC_LEFT, KC_DOWN, KC_RIGHT, _______, KC_QUOT},
-    {_______, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS, COLORCAP,_______, _______, KC_PAUS, KC_VOLD,  KC_VOLU, KC_MUTE},
-    {_______, _______, _______, _______, _______, _SPACE_, __NO___, _______, KC_SLCK, KC_MPRV,  KC_MNXT, KC_MPLY}
+  // layer 2: numbers
+  [2] = {
+    {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    _______, _______, _______, _______, _______,  _______,  KC_DEL },
+    {_______, KC_5,    KC_6,    KC_7,    KC_8,    _______, _______, _______, _______, _______,  _______,  _______},
+    {_______, KC_9,    KC_0,    KC_MINS, KC_EQL,  _______, _______, _______, _______, _______,  _______,  _______},
+    {_______, _______, _______, _______, _______, _SPACE_, __NO___, _______, _______, _______,  _______,  _______}
   },
-  [3] = {  /* Right FN */
-    {_______, KC_1,    KC_2,    KC_3,    KC_4,    _______, _______, KC_LBRC, KC_RBRC, KC_QUOT,  S(KC_QUOT), KC_BSPC},
-    {_______, KC_5,    KC_6,    KC_7,    KC_8,    _______, _______, KC_LCBR, KC_RCBR, KC_MINS,  KC_UNDS,   _______},
-    {_______, KC_9,    KC_0,    KC_MINS, KC_EQL,  _______, _______, KC_LPRN, KC_RPRN, _______,  _______,   _______},
-    {_______, _______, _______, _______, _______, _SPACE_, __NO___, _______, _______, _______,  _______,   _______}
+  // layer 3: symbols
+  [3] = {
+    {KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  _______, _______, KC_LBRC, KC_RBRC, _______,  _______,  KC_BSPC},
+    {_______, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, _______, _______, KC_LCBR, KC_RCBR, KC_MINS,  KC_UNDS,  _______},
+    {_______, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS, _______, _______, KC_LPRN, KC_RPRN, _______,  _______,  _______},
+    {_______, _______, _______, _______, _______, _SPACE_, __NO___, _______, _______, _______,  _______,  _______}
+  },
+  // layer 4: arrows and numbers
+  [4] = {
+    {_______, _______, _______, _______, _______, _______, _______, _______, KC_UP,   _______,  _______,   KC_DEL },
+    {_______, LEFTSCR, RIGHSCR,COLORCAP, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______,   M(1)   },
+    {_______, _______, _______, SCRNCAP, _______, _______, _______, KC_VOLD, KC_VOLU, KC_MUTE,  _______,   _______},
+    {_______, _______, _______, _______, _______, _SPACE_, __NO___, KC_MPRV, KC_MNXT, KC_MPLY,  _______,   _______}
   }
 };
 
