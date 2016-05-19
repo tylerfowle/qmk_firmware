90a781129b712c6ea6a862355965e278b7e00668
Tyler Fowle <tylerfowle@gmail.com>
2016-05-19 16:36:48 -0500

    add mouse layer

--- keyboard/planck/keymaps/tyler.c
+++ keyboard/planck/keymaps/tyler.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {M(0),    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,    KC_BSPC},
     {TAB_FN,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN, KC_ENT},
     {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT},
-    {TG(1),   KC_LCTL, KC_LALT, KC_LGUI, MO(3),   _SPACE_, __NO___, MO(2),   _______, KC_QUOT,  KC_BSLS, _______}
+    {TG(1),   KC_LCTL, KC_LALT, KC_LGUI, MO(3),   _SPACE_, __NO___, MO(2),   TG(5),   KC_QUOT,  KC_BSLS, _______}
   },
   // layer 1: base windows
   [1] = {
@@ -60,6 +60,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {_______, LEFTSCR, RIGHSCR,COLORCAP, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______,   M(1)   },
     {_______, _______, _______, SCRNCAP, _______, _______, _______, KC_VOLD, KC_VOLU, KC_MUTE,  _______,   _______},
     {_______, _______, _______, _______, _______, _SPACE_, __NO___, KC_MPRV, KC_MNXT, KC_MPLY,  _______,   _______}
+  },
+  // layer 5: mouse
+  [5] = {
+    {_______, _______, _______, _______, _______, _______, KC_WH_U, KC_BTN1, KC_MS_U, KC_BTN2,  _______,   _______},
+    {_______, _______, KC_ACL0, KC_ACL1, KC_ACL2, _______, KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R,  _______,   _______},
+    {_______, _______, _______, _______, _______, _______, _______, KC_BTN4, KC_BTN3, KC_BTN5,  _______,   _______},
+    {_______, _______, _______, _______, _______, _SPACE_, __NO___, _______, _______, _______,  _______,   _______}
   }
 };
 
