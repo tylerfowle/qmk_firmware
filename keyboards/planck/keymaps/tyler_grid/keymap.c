f809ce96c60338dfd270f47246d95c9d11067435
Tyler Fowle <tylerfowle@gmail.com>
2016-07-09 02:01:56 -0500

    add reset to bootload key, update startup sound variable name, switch pc mod keys around to default layout better. git comp git merge update

--- keyboards/planck/keymaps/tyler_grid/keymap.c
+++ keyboards/planck/keymaps/tyler_grid/keymap.c
@@ -32,42 +32,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {M(6),    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,    KC_BSPC},
     {TAB_FN,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN, KC_ENT},
     {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT},
-    {M(0),   KC_LCTL, KC_LALT, KC_LGUI, MO(3),   _SPACE_, _SPACE_, MO(2),   KC_QUOT, KC_BSLS,  _______, TG(5)}
+    {M(0),    KC_LCTL, KC_LALT, KC_LGUI, MO(3),   _SPACE_, _SPACE_, MO(2),   KC_QUOT, KC_BSLS,  _______, TG(5)}
   },
   // layer 1: base windows
   [1] = {
     {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______},
     {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______},
     {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______},
-    {M(1),   KC_LGUI, KC_LALT, KC_LCTL, _______, _______, _______, _______, _______, _______,  _______,  _______}
+    {M(1),    KC_LALT, KC_LGUI, KC_LCTL, _______, _______, _______, _______, _______, _______,  _______,  _______}
   },
   // layer 2: numbers
   [2] = {
     {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    _______, _______, _______, _______, _______,  _______,  KC_DEL },
     {_______, KC_5,    KC_6,    KC_7,    KC_8,    _______, _______, _______, _______, _______,  _______,  _______},
     {_______, KC_9,    KC_0,    KC_MINS, KC_EQL,  _______, _______, _______, _______, _______,  _______,  _______},
-    {M(2),   _______, _______, _______, _______, _SPACE_, __NO___, _______, _______, _______,  _______,  _______}
+    {M(2),    _______, _______, _______, _______, _SPACE_, _SPACE_, _______, _______, _______,  _______,  _______}
   },
   // layer 3: symbols
   [3] = {
     {KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  _______, _______, KC_LBRC, KC_RBRC, _______,  _______,  KC_BSPC},
     {_______, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, _______, _______, KC_LCBR, KC_RCBR, KC_MINS,  KC_UNDS,  _______},
     {_______, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS, _______, _______, KC_LPRN, KC_RPRN, _______,  _______,  _______},
-    {M(3),   _______, _______, _______, _______, _SPACE_, __NO___, _______, _______, _______,  _______,  _______}
+    {M(3),    _______, _______, _______, _______, _SPACE_, _SPACE_, _______, _______, _______,  _______,  _______}
   },
   // layer 4: arrows and numbers
   [4] = {
     {_______, _______, _______, _______, _______, _______, _______, _______, KC_UP,   _______,  _______,   KC_DEL },
     {_______, LEFTSCR, RIGHSCR,COLORCAP, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______,   M(7)   },
     {_______, _______, _______, SCRNCAP, _______, _______, _______, KC_VOLD, KC_VOLU, KC_MUTE,  _______,   _______},
-    {M(4),   _______, _______, _______, _______, _SPACE_, __NO___, KC_MPRV, KC_MNXT, KC_MPLY,  _______,   _______}
+    {M(4),    _______, _______, _______, _______, _SPACE_, _SPACE_, KC_MPRV, KC_MNXT, KC_MPLY,  _______,   RESET}
   },
   // layer 5: mouse
   [5] = {
     {_______, _______, _______, _______, _______, _______, KC_WH_U, KC_BTN1, KC_MS_U, KC_BTN2,  _______,   _______},
     {_______, _______, KC_ACL0, KC_ACL1, KC_ACL2, _______, KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R,  _______,   _______},
     {_______, _______, _______, _______, _______, _______, _______, KC_BTN4, KC_BTN3, KC_BTN5,  _______,   _______},
-    {M(5),   _______, _______, _______, _______, _SPACE_, __NO___, _______, _______, _______,  _______,   _______}
+    {M(5),    _______, _______, _______, _______, _SPACE_, _SPACE_, _______, _______, _______,  _______,   _______}
   }
 };
 
@@ -76,10 +76,9 @@ const uint16_t PROGMEM fn_actions[] = {
 };
 
 // Audio Stuff
-float tone_startup[][2]    = SONG(STARTUP_SOUND);
-
+float song_startup[][2]  = SONG(STARTUP_SOUND);
 float tone_0[][2] = {
-  ED_NOTE(_G2 )
+  ED_NOTE(_B1 )
 };
 float tone_1[][2] = {
   ED_NOTE(_GS3 )
@@ -98,12 +97,15 @@ float tone_5[][2] = {
 };
 
 
+
+
+
 void matrix_init_user(void) {
   startup_user();
 }
 void startup_user() {
   _delay_ms(20); // gets rid of tick
-  PLAY_NOTE_ARRAY(tone_startup, false, 0);
+  PLAY_NOTE_ARRAY(song_startup, false, 0);
 }
 
 void set_default_layer(uint32_t default_layer_state)
@@ -174,6 +176,8 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
       return MACRO( I(5), D(LSFT), T(SCLN), U(LSFT), T(W), T(Q), T(ENT), END  );
     }
     break;
+
+
 
   }
   return MACRO_NONE;
