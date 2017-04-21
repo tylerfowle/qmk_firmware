e8667c14f8ee98123c6399de2e13b83a686f2913
tylerfowle <tylerfowle@gmail.com>
2017-04-21 00:35:19 -0500

    update keymap, fix default layer cycle button, remove unused macros

--- keyboards/lets_split/keymaps/tyler_split/keymap.c
+++ keyboards/lets_split/keymaps/tyler_split/keymap.c
@@ -4,16 +4,6 @@
 
 extern keymap_config_t keymap_config;
 
-// Fillers to make layering more clear
-#define _______ KC_TRNS
-#define XXXXXXX KC_NO
-
-// Custom macros
-#define CTL_ESC     CTL_T(KC_ESC)               // Tap for Esc, hold for Ctrl
-#define SFT_ENT     SFT_T(KC_ENT)               // Tap for Enter, hold for Shift
-#define HPR_TAB     ALL_T(KC_TAB)               // Tap for Tab, hold for Hyper (Super+Ctrl+Alt+Shift)
-#define MEH_GRV     MEH_T(KC_GRV)               // Tap for Backtick, hold for Meh (Ctrl+Alt+Shift)
-
 //
 #define __NO___ KC_NO
 #define _______ KC_TRNS
@@ -29,45 +19,45 @@ extern keymap_config_t keymap_config;
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [0] = KEYMAP( \
-    M(6),    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,    KC_BSPC, \
+    M(0),    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,    KC_BSPC, \
     TAB_FN,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN, KC_ENT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT, \
-    TG(1),   KC_LCTL, KC_LALT, KC_LGUI, MO(3),   _SPACE_, _SPACE_, MO(2),   KC_QUOT, KC_BSLS,  _______, TG(5)
+    DF(1),   KC_LCTL, KC_LALT, KC_LGUI, MO(3),   _SPACE_, _SPACE_, MO(2),   KC_QUOT, KC_BSLS,  _______, TG(5)
   ),
 
   [1] = KEYMAP( \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, \
-    TG(0),   KC_LALT, KC_LGUI, KC_LCTL, _______, _______, _______, _______, _______, _______,  _______,  _______
+    DF(2),   KC_LALT, KC_LGUI, KC_LCTL, _______, _______, _______, _______, _______, _______,  _______,  _______
   ),
 
   [2] = KEYMAP( \
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    _______, _______, _______, _______, _______,  _______,  KC_DEL , \
     _______, KC_5,    KC_6,    KC_7,    KC_8,    _______, _______, _______, _______, _______,  _______,  _______, \
     _______, KC_9,    KC_0,    KC_MINS, KC_EQL,  _______, _______, _______, _______, _______,  _______,  _______, \
-    M(2),    _______, _______, _______, _______, _SPACE_, _SPACE_, _______, _______, _______,  _______,  _______
+    DF(3),    _______, _______, _______, _______, _SPACE_, _SPACE_, _______, _______, _______,  _______,  _______
   ),
 
   [3] = KEYMAP( \
     KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  _______, _______, KC_LBRC, KC_RBRC, _______,  _______,  KC_BSPC, \
     _______, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, _______, _______, KC_LCBR, KC_RCBR, KC_MINS,  KC_UNDS,  _______, \
     _______, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS, _______, _______, KC_LPRN, KC_RPRN, _______,  _______,  _______, \
-    M(3),    _______, _______, _______, _______, _SPACE_, _SPACE_, _______, _______, _______,  _______,  _______
+    DF(4),    _______, _______, _______, _______, _SPACE_, _SPACE_, _______, _______, _______,  _______,  _______
   ),
 
   [4] = KEYMAP( \
     _______, _______, _______, _______, _______, _______, _______, _______, KC_UP,   _______,  _______,   KC_DEL , \
-    _______, LEFTSCR, RIGHSCR,COLORCAP, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______,   M(7)   , \
+    _______, LEFTSCR, RIGHSCR,COLORCAP, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______,   M(1)   , \
     _______, _______, _______, SCRNCAP, _______, _______, _______, KC_VOLD, KC_VOLU, KC_MUTE,  _______,   _______, \
-    M(4),    _______, _______, _______, _______, _SPACE_, _SPACE_, KC_MPRV, KC_MNXT, KC_MPLY,  _______,   RESET
+    DF(5),    _______, _______, _______, _______, _SPACE_, _SPACE_, KC_MPRV, KC_MNXT, KC_MPLY,  _______,   RESET
   ),
 
   [5] = KEYMAP( \
     _______, _______, _______, _______, _______, _______, KC_WH_U, KC_BTN1, KC_MS_U, KC_BTN2,  _______,   RESET, \
     _______, _______, KC_ACL0, KC_ACL1, KC_ACL2, _______, KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R,  _______,   _______, \
     _______, _______, _______, _______, _______, _______, _______, KC_BTN4, KC_BTN3, KC_BTN5,  _______,   _______, \
-    M(5),    _______, _______, _______, _______, _SPACE_, _SPACE_, _______, _______, _______,  _______,   _______
+    DF(0),    _______, _______, _______, _______, _SPACE_, _SPACE_, _______, _______, _______,  _______,   _______
   )
 
 };
