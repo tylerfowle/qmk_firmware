a2705aef6fa583352fece840cedb6dc3acd3367d
tylerfowle <tylerfowle@gmail.com>
2017-05-04 13:21:44 -0500

    add persistent default layer switching, todo: turn off mouse layer after layer change

--- keyboards/lets_split/keymaps/tyler_split/keymap.c
+++ keyboards/lets_split/keymaps/tyler_split/keymap.c
@@ -8,7 +8,22 @@ extern keymap_config_t keymap_config;
 // leader key
 LEADER_EXTERNS();
 
+#define _MAC  0
+#define _WIN  1
+#define _NUM 2
+#define _SYM 3
+#define _ARROW 4
+#define _MOUSE 5
 
+
+enum custom_keycodes {
+  MAC = SAFE_RANGE,
+  WIN,
+  NUM,
+  SYM,
+  ARROW,
+  MOUSE
+};
 
 
 // for readability
@@ -27,54 +42,47 @@ LEADER_EXTERNS();
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [0] = KEYMAP( \
-    M(0),    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,    KC_BSPC, \
-    TAB_FN,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN, KC_ENT, \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT, \
-    KC_LCTL, KC_LCTL, KC_LALT, KC_LGUI, MO(3),   HYPRSPC, _SPACE_, MO(2),   KC_QUOT, KC_BSLS,  KC_LEAD, TG(5)
-  ),
+  [_MAC] = KEYMAP( \
+      M(0),    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,    KC_BSPC, \
+      TAB_FN,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN, KC_ENT, \
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT, \
+      KC_LCTL, KC_LCTL, KC_LALT, KC_LGUI, MO(3),   HYPRSPC, _SPACE_, MO(2),   KC_QUOT, KC_BSLS,  KC_LEAD, TG(5)
+      ),
 
-  [1] = KEYMAP( \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, \
-    _______, KC_LALT, KC_LGUI, KC_LCTL, _______, _______, _______, _______, _______, _______,  _______,  _______
-  ),
+  [_WIN] = KEYMAP( \
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, \
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, \
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, \
+      _______, KC_LALT, KC_LGUI, KC_LCTL, _______, _______, _______, _______, _______, _______,  _______,  _______
+      ),
 
-  [2] = KEYMAP( \
-    _______, KC_1,    KC_2,    KC_3,    KC_4,    _______, _______, _______, _______, _______,  _______,  KC_DEL , \
-    _______, KC_5,    KC_6,    KC_7,    KC_8,    _______, _______, _______, _______, _______,  _______,  _______, \
-    _______, KC_9,    KC_0,    KC_MINS, KC_EQL,  _______, _______, _______, _______, _______,  _______,  _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______
-  ),
+  [_NUM] = KEYMAP( \
+      _______, KC_1,    KC_2,    KC_3,    KC_4,    _______, _______, _______, _______, _______,  _______,  KC_DEL , \
+      _______, KC_5,    KC_6,    KC_7,    KC_8,    _______, _______, _______, _______, _______,  _______,  _______, \
+      _______, KC_9,    KC_0,    KC_MINS, KC_EQL,  _______, _______, _______, _______, _______,  _______,  _______, \
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______
+      ),
 
-  [3] = KEYMAP( \
-    _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  _______, _______, KC_LBRC, KC_RBRC, _______,  _______,  KC_BSPC, \
-    _______, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, _______, _______, KC_LCBR, KC_RCBR, KC_MINS,  KC_UNDS,  _______, \
-    _______, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS, _______, _______, KC_LPRN, KC_RPRN, _______,  _______,  _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______
-  ),
+  [_SYM] = KEYMAP( \
+      _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  _______, _______, KC_LBRC, KC_RBRC, _______,  _______,  KC_BSPC, \
+      _______, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, _______, _______, KC_LCBR, KC_RCBR, KC_MINS,  KC_UNDS,  _______, \
+      _______, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS, _______, _______, KC_LPRN, KC_RPRN, _______,  _______,  _______, \
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______
+      ),
 
-  [4] = KEYMAP( \
-    _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_UP,   KC_END,   _______,   KC_DEL , \
-    _______, LEFTSCR, RIGHSCR,COLORCAP, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______,   M(1)   , \
-    _______, _______, _______, SCRNCAP, _______, _______, _______, KC_VOLD, KC_VOLU, KC_MUTE,  _______,   _______, \
-    _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MNXT, KC_MPLY,  _______,   _______
-  ),
+  [_ARROW] = KEYMAP( \
+      _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_UP,   KC_END,   _______,   KC_DEL , \
+      _______, LEFTSCR, RIGHSCR,COLORCAP, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______,   M(1)   , \
+      _______, _______, _______, SCRNCAP, _______, _______, _______, KC_VOLD, KC_VOLU, KC_MUTE,  _______,   _______, \
+      _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MNXT, KC_MPLY,  _______,   _______
+      ),
 
-  [5] = KEYMAP( \
-    _______, _______, _______, _______, _______, _______, KC_WH_U, KC_BTN1, KC_MS_U, KC_BTN2,  _______,   RESET, \
-    _______, _______, KC_ACL0, KC_ACL1, KC_ACL2, _______, KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R,  _______,   _______, \
-    _______, _______, _______, _______, _______, _______, _______, KC_BTN4, KC_BTN3, KC_BTN5,  _______,   _______, \
-    DF(0),   DF(1),   DF(2),   DF(3),   DF(4),   _______, _______, _______, _______, _______,  _______,   _______
-  ),
-
-  [6] = KEYMAP( \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______
-  )
+  [_MOUSE] = KEYMAP( \
+      _______, _______, _______, _______, _______, _______, KC_WH_U, KC_BTN1, KC_MS_U, KC_BTN2,  _______,   RESET, \
+      _______, _______, KC_ACL0, KC_ACL1, KC_ACL2, _______, KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R,  _______,   _______, \
+      _______, _______, _______, _______, _______, _______, _______, KC_BTN4, KC_BTN3, KC_BTN5,  _______,   _______, \
+      MAC,     WIN,     NUM,     SYM,     ARROW,  _______, _______, _______, _______, _______,  _______,   _______
+      )
 
 };
 
@@ -83,22 +91,22 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
   switch(id) {
     case 0: {
-      static uint8_t code;
-      if (record->event.pressed) {
-        code = (keyboard_report->mods & (MOD_BIT(KC_LSFT) | MOD_BIT(KC_RSFT))) ? KC_GRV : KC_ESC;
-        register_code(code);
-      }
-      else {
-        unregister_code(code);
-      }
-      break;
-    }
-    break;
+              static uint8_t code;
+              if (record->event.pressed) {
+                code = (keyboard_report->mods & (MOD_BIT(KC_LSFT) | MOD_BIT(KC_RSFT))) ? KC_GRV : KC_ESC;
+                register_code(code);
+              }
+              else {
+                unregister_code(code);
+              }
+              break;
+            }
+            break;
     case 1:
-    if (record->event.pressed) {
-      return MACRO( I(5), D(LSFT), T(SCLN), U(LSFT), T(W), T(Q), T(ENT), END  );
-    }
-    break;
+            if (record->event.pressed) {
+              return MACRO( I(5), D(LSFT), T(SCLN), U(LSFT), T(W), T(Q), T(ENT), END  );
+            }
+            break;
   }
   return MACRO_NONE;
 };
@@ -132,29 +140,55 @@ void matrix_scan_user(void) {
       SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
     }
 
-    // function
-    SEQ_ONE_KEY (KC_F) {
-      SEND_STRING ( "function(){" );
-      register_code ( KC_ENT );
-      register_code ( KC_DOWN );
-      register_code ( KC_RIGHT );
-      SEND_STRING ( ");" );
-      register_code ( KC_UP );
-      SEND_STRING ( "  " );
+  }
+}
 
-      unregister_code ( KC_ENT );
-      unregister_code ( KC_DOWN );
-      unregister_code ( KC_RIGHT );
-      unregister_code ( KC_UP );
-    }
 
-    SEQ_ONE_KEY (KC_T) {
-      SEND_STRING ( "$(this)" );
-    }
+void persistant_default_layer_set(uint16_t default_layer) {
+  eeconfig_update_default_layer(default_layer);
+  default_layer_set(default_layer);
+}
 
-    SEQ_ONE_KEY (KC_E) {
-      SEND_STRING ( ".each()" );
-    }
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case MAC:
+      if (record->event.pressed) {
+        persistant_default_layer_set(1UL<<_MAC);
+      }
+      return false;
+      break;
+    case WIN:
+      if (record->event.pressed) {
+        persistant_default_layer_set(1UL<<_WIN);
+      }
+      return false;
+      break;
+    case NUM:
+      if (record->event.pressed) {
+        persistant_default_layer_set(1UL<<_NUM);
+      }
+      return false;
+      break;
+    case SYM:
+      if (record->event.pressed) {
+        persistant_default_layer_set(1UL<<_SYM);
+      }
+      return false;
+      break;
+    case ARROW:
+      if (record->event.pressed) {
+        persistant_default_layer_set(1UL<<_ARROW);
+      }
+      return false;
+      break;
+    case MOUSE:
+      if (record->event.pressed) {
+        persistant_default_layer_set(1UL<<_MOUSE);
+      }
+      return false;
+      break;
 
   }
+  return true;
 }
