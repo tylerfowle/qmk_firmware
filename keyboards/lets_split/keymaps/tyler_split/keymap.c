a3a5d56ce895a06766447c6ff3e81caddfdef89e
tylerfowle <tylerfowle@gmail.com>
2017-06-07 14:06:58 -0500

    LT is too slow for main layer keys, switched them back and added quote, dquote and grv to number layer

--- keyboards/lets_split/keymaps/tyler_split/keymap.c
+++ keyboards/lets_split/keymaps/tyler_split/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       M(0),    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,    KC_BSPC, \
       TAB_FN,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN, KC_ENT, \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT, \
-      KC_LCTL, KC_LCTL, KC_LALT, KC_LGUI, L_LAYER, HYPRSPC, _SPACE_, R_LAYER, KC_DQUO, KC_BSLS,  KC_LEAD, TG(5)
+      KC_LCTL, KC_LCTL, KC_LALT, KC_LGUI, MO(3),   HYPRSPC, _SPACE_, MO(2),   KC_DQUO, KC_BSLS,  KC_LEAD, TG(5)
       ),
 
   [_WIN] = KEYMAP( \
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_NUM] = KEYMAP( \
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    _______, _______, _______, _______, _______,  _______,  KC_DEL , \
-      _______, KC_5,    KC_6,    KC_7,    KC_8,    _______, _______, _______, _______, _______,  _______,  _______, \
+      _______, KC_5,    KC_6,    KC_7,    KC_8,    _______, _______, KC_QUOT, KC_DQUO, KC_GRV,   _______,  _______, \
       _______, KC_9,    KC_0,    KC_MINS, KC_EQL,  _______, _______, _______, _______, _______,  _______,  _______, \
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______
       ),
