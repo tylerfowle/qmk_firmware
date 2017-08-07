cd2104aea771af0e299787a90c3d69610fd52350
tylerfowle <tylerfowle@gmail.com>
2017-08-07 15:13:41 -0500

    remove leader key, replace with f20

--- keyboards/lets_split/keymaps/tyler_split/keymap.c
+++ keyboards/lets_split/keymaps/tyler_split/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       M(0),    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,    KC_BSPC, \
       TAB_FN,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN, KC_ENT, \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT, \
-      KC_LCTL, KC_LCTL, KC_LALT, KC_LGUI, MO(3),   HYPRSPC, _SPACE_, MO(2),   MO(6),   KC_BSLS,  KC_LEAD, TG(5)
+      KC_LCTL, KC_LCTL, KC_LALT, KC_LGUI, MO(3),   HYPRSPC, _SPACE_, MO(2),   MO(6),   KC_BSLS,  KC_F20,  TG(5)
       ),
 
   [_WIN] = KEYMAP( \
