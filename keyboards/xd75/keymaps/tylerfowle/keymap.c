dac7c89156870d4f04026c9f03dc962cb1381e76
tylerfowle <tylerfowle@gmail.com>
2018-02-02 19:06:56 -0600

    add arrow cluster to keymap

--- keyboards/xd75/keymaps/tylerfowle/keymap.c
+++ keyboards/xd75/keymaps/tylerfowle/keymap.c
@@ -53,11 +53,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* 0 */
   [_MAC] = {
-    { M(0),    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______, _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC },
-    { _______, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______, _______, _______, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    _______ },
-    { TAB_FN,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    _______, _______, _______, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT  },
-    { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______, _______, _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT },
-    { KC_LCTL, KC_LCTL, KC_LALT, KC_LGUI, MO(_SYM),HYPRSPC, _______, _______, _______, _SPACE_, MO(_NUM),MO(_EXT),KC_BSLS, KC_LEAD, TG(_MOUSE) },
+    { M(0),    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, _______, _______, KC_BSPC },
+    { KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_INS,  KC_HOME, KC_PGUP, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    _______ },
+    { TAB_FN,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_DEL,  KC_END,  KC_PGDN, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT  },
+    { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_P0,   KC_UP,   KC_PDOT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT },
+    { KC_LCTL, KC_LCTL, KC_LALT, KC_LGUI, MO(_SYM),_SPACE_, KC_LEFT, KC_DOWN, KC_RIGHT, _SPACE_, MO(_NUM),MO(_EXT),KC_BSLS, KC_LEAD, TG(_MOUSE) },
   },
 
   /* 1 */
@@ -72,19 +72,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* 2 */
   [_NUM] = {
     { KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_DEL  },
-    { _______, KC_1,    KC_2,    KC_3,    KC_4,    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ },
-    { _______, KC_5,    KC_6,    KC_7,    KC_8,    _______, _______, _______, _______, _______, KC_QUOT, KC_DQUO, KC_GRV,  _______, _______ },
-    { _______, KC_9,    KC_0,    KC_MINS, KC_EQL,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ },
-    { _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ },
+    { _______, KC_1,    KC_2,    KC_3,    KC_4,    _______, KC_P7,   KC_P8,   KC_P9,   _______, _______, _______, _______, _______, _______ },
+    { _______, KC_5,    KC_6,    KC_7,    KC_8,    _______, KC_P4,   KC_P5,   KC_P6,   _______, KC_QUOT, KC_DQUO, KC_GRV,  _______, _______ },
+    { _______, KC_9,    KC_0,    KC_MINS, KC_EQL,  _______, KC_P1,   KC_P2,   KC_P3,   _______, _______, _______, _______, _______, _______ },
+    { _______, _______, _______, _______, _______, _______, KC_P0,   KC_PPLS, KC_PDOT, _______, _______, _______, _______, _______, _______ },
   },
 
   /* 3 */
   [_SYM] = {
     { KC_GRV,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BSPC},
-    { _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, _______, _______, _______ },
-    { _______, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, _______, _______, _______, _______, _______, KC_LCBR, KC_RCBR, KC_MINS, KC_UNDS, _______ },
-    { _______, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS, _______, _______, _______, _______, _______, KC_LPRN, KC_RPRN, _______, _______, _______ },
-    { _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ },
+    { _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  _______, KC_P7,   KC_P8,   KC_P9,   _______, KC_LBRC, KC_RBRC, _______, _______, _______ },
+    { _______, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, _______, KC_P4,   KC_P5,   KC_P6,   _______, KC_LCBR, KC_RCBR, KC_MINS, KC_UNDS, _______ },
+    { _______, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS, _______, KC_P1,   KC_P2,   KC_P3,   _______, KC_LPRN, KC_RPRN, _______, _______, _______ },
+    { _______, _______, _______, _______, _______, _______, KC_P0,   KC_PPLS, KC_PDOT, _______, _______, _______, _______, _______, _______ },
   },
 
   /* 4 */
