392d3091dafc5bc49b87c57ee2f64c027542ffd5
tylerfowle <tylerfowle@gmail.com>
2020-01-13 15:24:44 -0600

    add another pipe

--- keyboards/xd75/keymaps/tylerfowle/keymap.c
+++ keyboards/xd75/keymaps/tylerfowle/keymap.c
@@ -42,6 +42,8 @@ enum custom_keycodes {
 #define LEFTSCR LALT(KC_1)
 #define RIGHSCR LALT(KC_2)
 
+#define PIPE LSFT(KC_BSLS)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* { _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ }, */
   /* _______, KC_F13,  KC_F14,  KC_F15,  KC_F16,  _______, _______, _______, _______, _______,  _______,   _______ */
@@ -67,18 +69,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* 2 */
   [_NUM] = {
-    { KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,         KC_F10,  KC_F11,  KC_F12,  _______, KC_DEL  },
-    { _______, KC_1,    KC_2,    KC_3,    KC_4,    _______, KC_P7,   KC_P8,   KC_P9,   _______,       _______, _______, _______, _______, _______ },
-    { _______, KC_5,    KC_6,    KC_7,    KC_8,    _______, KC_P4,   KC_P5,   KC_P6,   LSFT(KC_BSLS), KC_QUOT, KC_DQUO, KC_GRV,  _______, _______ },
-    { _______, KC_9,    KC_0,    KC_MINS, KC_EQL,  _______, KC_P1,   KC_P2,   KC_P3,   _______,       _______, _______, _______, _______, _______ },
-    { _______, _______, _______, _______, _______, _______, KC_P0,   KC_PPLS, KC_PDOT, _______,       _______, _______, _______, _______, _______ },
+    { KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_DEL  },
+    { _______, KC_1,    KC_2,    KC_3,    KC_4,    _______, KC_P7,   KC_P8,   KC_P9,   _______, _______, _______, _______, _______, _______ },
+    { _______, KC_5,    KC_6,    KC_7,    KC_8,    _______, KC_P4,   KC_P5,   KC_P6,   PIPE,    KC_QUOT, KC_DQUO, KC_GRV,  _______, _______ },
+    { _______, KC_9,    KC_0,    KC_MINS, KC_EQL,  _______, KC_P1,   KC_P2,   KC_P3,   _______, _______, _______, _______, _______, _______ },
+    { _______, _______, _______, _______, _______, _______, KC_P0,   KC_PPLS, KC_PDOT, _______, _______, _______, _______, _______, _______ },
   },
 
   /* 3 */
   [_SYM] = {
     { KC_GRV,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BSPC},
     { _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  _______, KC_P7,   KC_P8,   KC_P9,   _______, KC_LBRC, KC_RBRC, _______, _______, _______ },
-    { _______, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, _______, KC_P4,   KC_P5,   KC_P6,   _______, KC_LCBR, KC_RCBR, KC_MINS, KC_UNDS, _______ },
+    { _______, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, _______, KC_P4,   KC_P5,   KC_P6,   PIPE,    KC_LCBR, KC_RCBR, KC_MINS, KC_UNDS, _______ },
     { _______, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS, _______, KC_P1,   KC_P2,   KC_P3,   _______, KC_LPRN, KC_RPRN, _______, _______, _______ },
     { _______, _______, _______, _______, _______, _______, KC_P0,   KC_PPLS, KC_PDOT, _______, _______, _______, _______, _______, _______ },
   },
